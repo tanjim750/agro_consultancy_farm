6ab9eb5f2321254f3ac0ccf2bdf7a51c31111b37
Tanjim Abubokor <59773799+tanjim750@users.noreply.github.com>
2022-12-13 10:50:28 +0600

    Add files via upload

---
+++ agro_farm/agro_farm/urls.py
@@ -0,0 +1,44 @@
+"""agro_farm URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path , include
+from django.contrib.auth import views as auth_views
+
+
+#For Send html template to email
+from django.core.mail import EmailMultiAlternatives
+from django.utils.html import strip_tags
+from django.template.loader import render_to_string
+
+
+urlpatterns = [
+    path('mysite/admin', admin.site.urls),
+path('password-reset/',
+         auth_views.PasswordResetView.as_view(subject_template_name='subject.txt'),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
+    path('', include('farm.urls')),
+
+
+]
