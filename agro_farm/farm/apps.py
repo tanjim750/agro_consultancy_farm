2f730b30e06aea0670700ed5c24fa388cd553b81
Tanjim Abubokor <59773799+tanjim750@users.noreply.github.com>
2023-03-07 13:47:33 +0600

    Delete agro_farm directory

--- agro_farm/farm/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class FarmConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'farm'
-
-    def ready(self):
-        import farm.singlas
